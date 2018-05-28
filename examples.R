dd9325e6404cf791788e548f96345209efe51b8e
James Harper <james.harper.us@gmail.com>
2018-05-28 16:22:56 -0600

    Updated examples.R

--- examples.R
+++ examples.R
@@ -10,3 +10,31 @@ pet = "dog"
 x <- 1
 pet <- "dog"
 
+###############################################################################
+# INITIALIZE
+###############################################################################
+rm(list = ls())                                      # Clear global environment
+cat("\014")                                          # Clear console window
+file.remove(dir(paste(getwd(),"/output/", sep = ""), # Clear output folder
+                full.names = TRUE))    
+source("functions.R")                                # Load custom functions
+load_libraries(c("rio", "Amelia"))                   # Install & load libraries
+
+###############################################################################
+# LOAD DATA
+###############################################################################
+file.to.import = paste(getwd(), "/data/data_oct2017.xlsx", sep = "")
+data = import(file.to.import)
+# names(data)
+
+###############################################################################
+# CLEAN DATA
+###############################################################################
+old.col.names = names(data)                        # Save original column names
+names(data) = c("ID", "Creatr", "Date")            # Change column names
+print(data.frame(names(data), var_desc))           # Verify consistency
+
+# See presentation for more examples of cleaning data
+
+
+
